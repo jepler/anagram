122d3261d66a3a747a7c3de4117d9916c383ee90
Jeff Epler <jepler@unpythonic.net>
2013-02-19 16:11:49 -0600

    remove debugging messages

--- anagram.js
+++ anagram.js
@@ -1,25 +1,21 @@
 var request = null, lastquery = '';
 
 function updatejax(data) {
-console.log('updatejax');
     $('#results').text(data);
 }
 
 function clearjax() {
-console.log('clearjax');
     var query = $('#query').val();
     if (query != lastquery) queuejax();
     else request = null;
 }
 
 function queuejax () {
-console.log('queuejax');
     var query = $('#query').val();
     lastquery = query;
     var loc = (
         document.location.toString().replace(/\?.*$/, "")
         + '?' + $.param({'p': 1, 'q': query + ' -50'}));
-console.log(loc);
     request = $.ajax({'url': loc, 'cache': true})
         .done(updatejax)
         .always(clearjax);
@@ -29,22 +25,18 @@ function fulljax (e) {
     if(e) e.preventDefault();
     if(request) request.abort();
 
-console.log('fulljax');
     var query = $('#query').val();
     lastquery = query;
     var loc = (
         document.location.toString().replace(/\?.*$/, "")
         + '?' + $.param({'p': 1, 'q': query}));
-console.log(loc);
     request = $.ajax({'url': loc, 'cache': true})
         .done(updatejax)
         .always(clearjax);
-console.log('fulljax returning false');
     return false;
 }
 
 function mayjax() {
-console.log('mayjax');
     if(request) return;
     var query = $('#query').val();
     if(query == '' || query == lastquery) return;
